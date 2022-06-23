973c8157ee1b8cd7e88ad30e95bd7deb962727d4
smandla <35283559+smandla@users.noreply.github.com>
2022-06-22 23:31:44 -0700

    Add few questions and change UI based on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,50 @@
  */
 let startGameBtn = document.querySelector("#start_quiz");
 let nameVal = document.getElementById("name");
-let questionField = document.getElementById("question_field");
-let questionNumber = document.getElementById("question_number");
+let question_field = document.getElementById("question_field");
+let question_number = document.getElementById("question_number");
 let answerBox = document.getElementById("answer");
-let timerView = document.getElementById("timer_seconds");
+let timer_view = document.getElementById("timer_seconds");
 
 var savedName;
+let questionNum = 1;
+let quizBank = [
+  {
+    question: "What is not a data type supported by JavaScript?",
+    options: ["Boolean", "String", "Object", "Stream"],
+    answer: "Stream",
+  },
+  {
+    question: "What is the right syntax for an object?",
+    options: ["[ ]", "( )", "{ }", "' '"],
+    answer: "{ }",
+  },
+  {
+    question: "Which type of variable can be modified?",
+    options: ["var", "const", "constant", "let"],
+    answer: "let",
+  },
+  {
+    question: 'What is \'console.log(5 + 2 + "3")',
+    options: ["103", "10", "7 + '3'", "73"],
+    answer: "73",
+  },
+  {
+    question: "How do you do a multiline comment?",
+    options: ["//", "/** **/", "*/ /*", "//* *//"],
+    answer: "/** **/",
+  },
+];
+let totalQs = quizBank.length;
+let maxSeconds = 25;
 const checkAndSaveInput = () => {};
-
+const showQuestions = () => {
+  // show question_numbers
+  question_number.innerHTML = "Question " + questionNum + " of " + totalQs;
+  timer_view.innerHTML = maxSeconds + "s";
+  question_field.innerHTML = quizBank[questionNum - 1].question;
+  nameVal.remove();
+};
 const playGame = () => {
   if (nameVal.value) {
     savedName = nameVal.value;
